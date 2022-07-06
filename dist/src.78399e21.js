4070a0473682cf12038521d1e959aa1c4dd345b3
mcb7729 <matthewcbroussard@gmail.com>
2022-07-06 08:08:55 -0500

    Update src.78399e21.js
    re-installed parcel

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -33237,7 +33237,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56397" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57977" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
